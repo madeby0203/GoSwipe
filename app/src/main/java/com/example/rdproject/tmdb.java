f1d127f7cc7089bfdee4a7b3794a9bd33655ff88
tderksen <thomderksen@gmail.com>
2021-04-21 14:16:23 +0200

    Test
    (cherry picked from commit 52f5f5ac980cf06e63f270f61b86fad434f6ad88)

---
+++ app/src/main/java/com/example/rdproject/tmdb.java
@@ -0,0 +1,19 @@
+package com.example.rdproject;
+
+public class tmdb {
+
+    public String apiKey;
+
+    private void tmbd(String key) {
+        this.apiKey = key;
+    }
+
+    private boolean connect() {
+        return true;
+    }
+
+    private void movie(int id) {
+
+    }
+
+}
