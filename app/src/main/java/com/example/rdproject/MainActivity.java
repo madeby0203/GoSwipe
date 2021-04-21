72fe546a42040a0579b15d6859dffc436f91af47
lesselink <l.esselink@student.science.ru.nl>
2021-04-21 14:51:19 +0200

    De mark is cool button

--- app/src/main/java/com/example/rdproject/MainActivity.java
+++ app/src/main/java/com/example/rdproject/MainActivity.java
@@ -1,16 +1,36 @@
 package com.example.rdproject;
 
+import android.annotation.SuppressLint;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextClock;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Boolean mark_is_cool = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        boolean marciscool = true;
-        if (marciscool){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void testclick(View view){
+        TextView testtxt = findViewById(R.id.testtxt);
+        Button testbutton = findViewById(R.id.testbutton);
+
+        if (mark_is_cool) {
+            testtxt.setText("Mark is cool");
+            testbutton.setText("Niet cool!");
+            mark_is_cool = false;
+        } else {
+            testtxt.setText("Mark is niet cool");
+            testbutton.setText("Wel cool!");
+            mark_is_cool = true;
         }
     }
 
