3b13b550c0ffbbcbfb7426da525d45b5165cb719
mfijneman <m.fijneman@student.science.ru.nl>
2021-05-13 16:03:16 +0200

    Added usernames
    (cherry picked from commit cd8683660ca135c18175d3c2a941986ff56e63e7)

--- app/src/main/java/rd/project/MainActivity.java
+++ app/src/main/java/rd/project/MainActivity.java
@@ -1,8 +1,11 @@
 package rd.project;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import rd.project.fragments.MenuFragment;
+import rd.project.fragments.NameFragment;
 
 public class MainActivity extends AppCompatActivity {
     
@@ -16,10 +19,42 @@ public class MainActivity extends AppCompatActivity {
         
         // Switch to the initial fragment: the menu fragment
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .setReorderingAllowed(true)
-                    .add(R.id.fragment_container_view, MenuFragment.class, null)
-                    .commit();
+            String username = getUsername();    // Retrieve username
+            if(username == null) {  // Username not set, ask user for one
+                getSupportFragmentManager().beginTransaction()
+                        .setReorderingAllowed(true)
+                        .add(R.id.fragment_container_view, NameFragment.class, null)
+                        .commit();
+            } else {    // Username set, go to main menu
+                getSupportFragmentManager().beginTransaction()
+                        .setReorderingAllowed(true)
+                        .add(R.id.fragment_container_view, MenuFragment.class, null)
+                        .commit();
+            }
         }
+    }
+    
+    /**
+     * Retrieves username from SharedPreferences
+     * @return username
+     */
+    public String getUsername() {
+        SharedPreferences prefs = getApplicationContext()
+                .getSharedPreferences(getString(R.string.preferences_name), Context.MODE_PRIVATE);
+        
+        return prefs.getString(getString(R.string.username_preference), null);
+    }
+    
+    /**
+     * Sets username in SharedPreferences
+     * @param name username
+     */
+    public void setUsername(String name) {
+        SharedPreferences prefs = getApplicationContext()
+                .getSharedPreferences(getString(R.string.preferences_name), Context.MODE_PRIVATE);
+        
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(getString(R.string.username_preference), name)
+                .apply();
     }
 }
