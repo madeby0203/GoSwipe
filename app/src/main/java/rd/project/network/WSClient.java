df3d57e54e11c9a5180d93562501a42b0dd59758
mfijneman <m.fijneman@student.science.ru.nl>
2021-05-24 10:28:18 +0200

    Logging improvements for WebSocket Client and Server

--- app/src/main/java/rd/project/network/WSClient.java
+++ app/src/main/java/rd/project/network/WSClient.java
@@ -1,5 +1,6 @@
 package rd.project.network;
 
+import android.util.Log;
 import org.greenrobot.eventbus.EventBus;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.drafts.Draft_6455;
@@ -9,6 +10,7 @@ import rd.project.events.WSClientEvent;
 import java.net.URI;
 
 public class WSClient extends WebSocketClient {
+    private final String TAG = "WSClient";
     
     public WSClient(URI serverURI) {
         super(serverURI, new Draft_6455(), null, 4000);
@@ -16,21 +18,26 @@ public class WSClient extends WebSocketClient {
     
     @Override
     public void onOpen(ServerHandshake handshakedata) {
+        Log.d(TAG, "Connection opened.");
         EventBus.getDefault().post(new WSClientEvent.Open());
     }
     
     @Override
     public void onMessage(String message) {
+        Log.d(TAG, "Received message: " + message);
         EventBus.getDefault().post(new WSClientEvent.Message(message));
     }
     
     @Override
     public void onClose(int code, String reason, boolean remote) {
+        Log.d(TAG, "Connection closed. code: " + code + ", reason: " + reason + ", remote: " + remote);
         EventBus.getDefault().post(new WSClientEvent.Close(reason));
     }
     
     @Override
     public void onError(Exception ex) {
+        Log.e(TAG, "Error opening connection.");
+        ex.printStackTrace();
         EventBus.getDefault().post(new WSClientEvent.Error(ex));
     }
 }
